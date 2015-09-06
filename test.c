6662f6a2e77909e6cf4419b2ab13d8f9f8ade1ca
Heng Li <lh3@me.com>
2015-09-05 23:35:33 -0400

    use 128-bit in test
    64-bit may be gone. The 128-bit version is as fast.

--- test.c
+++ test.c
@@ -11,6 +11,7 @@ int main(int argc, char *argv[])
 	kseq_t *seq;
 	int c, w = 20, k = 17;
 	uint64_t shift, mask;
+	mm128_v a = {0,0,0};
 	while ((c = getopt(argc, argv, "w:k:")) >= 0) {
 		if (c == 'w') w = atoi(optarg);
 		else if (c == 'k') k = atoi(optarg);
@@ -19,14 +20,12 @@ int main(int argc, char *argv[])
 	fp = (strcmp(argv[optind], "-") == 0)? gzdopen(fileno(stdin), "r") : gzopen(argv[optind], "r");
 	seq = kseq_init(fp);
 	while (kseq_read(seq) >= 0) {
-		int i, n;
-		uint64_t *a;
-		a = mm_sketch64(seq->seq.s, seq->seq.l, w, k, &n);
+		int i;
+		mm_sketch128(seq->seq.s, seq->seq.l, w, k, &a);
 		printf(">%s\n", seq->name.s);
-		for (i = 0; i < n; ++i)
-			printf("%llu\t%llx\n", a[i]>>shift, a[i]&mask);
+		for (i = 0; i < a.n; ++i)
+			printf("%llu\t%llx\n", a.a[i].y, a.a[i].x);
 		printf("//\n");
-		free(a);
 	}
 	kseq_destroy(seq);
 	gzclose(fp);
