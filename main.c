7f6cd5b64f91a822d36b68d916efe81a8ec95952
Heng Li <lh3@me.com>
2015-09-10 10:36:54 -0400

    r42: reduced -f to 0.1%

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 #include <sys/time.h>
 #include "minimap.h"
 
-#define MM_VERSION "r39"
+#define MM_VERSION "r42"
 
 void liftrlimit()
 {
@@ -21,7 +21,7 @@ void liftrlimit()
 int main(int argc, char *argv[])
 {
 	int i, c, k = 15, w = -1, b = 14, radius = 1000, min_cnt = 3, n_threads = 3, batch_size = 10000000, keep_name = 1;
-	float f = 0.005;
+	float f = 0.001;
 	mm_idx_t *mi = 0;
 
 	liftrlimit();
