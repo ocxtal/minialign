849c350c79e96240dc0903fd6827979b1f8b755a
Hajime Suzuki <yline5050hs@gmail.com>
2017-01-30 23:03:53 +0900

    fix input redirection

--- minialign.c
+++ minialign.c
@@ -1686,7 +1686,7 @@ int main(int argc, char *argv[])
 		case 2: mm_print_help(opt); ret = 0; goto _final;
 	}
 	if (!fnr && v.n == 0) { mm_print_help(opt); ret = 1; goto _final; }
-	if ((fnr && v.n == 0) || (!fnr && v.n == 1 && !(opt->flag&MM_AVA))) {
+	if (!fnw && ((fnr && v.n == 0) || (!fnr && v.n == 1 && !(opt->flag&MM_AVA)))) {
 		fprintf(stderr, "[M::%s] query-side input redirected to stdin.\n", __func__);
 		kv_push(void*, v, "-");
 	}
