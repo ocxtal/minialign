86ca61d93a6a1411869b5de03a19070bb8dbde0e
Hajime Suzuki <yline5050hs@gmail.com>
2017-02-03 00:42:16 +0900

    fix load balancing param

--- minialign.c
+++ minialign.c
@@ -353,7 +353,7 @@ static int pt_set_worker(pt_t *pt, pt_worker_t wfp, void **warg)
 static int pt_stream(pt_t *pt, pt_source_t sfp, void *sarg, pt_worker_t wfp, void **warg, pt_drain_t dfp, void *darg)
 {
 	if (pt_set_worker(pt, wfp, warg)) return -1;
-	uint64_t bal = 0, lb = pt->nth, ub = 4 * pt->nth;
+	uint64_t bal = 0, lb = 2 * pt->nth, ub = 4 * pt->nth;
 	void *item, *arr[ub];
 	while ((item = sfp(sarg)) != NULL) {
 		pt_enq(pt->c->in, pt->c->tid, item);
