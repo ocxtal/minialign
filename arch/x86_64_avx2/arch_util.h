d919fd257da29e7f25f66a109ae696d381b5076f
Hajime Suzuki <yline5050hs@gmail.com>
2018-02-06 19:22:38 +0900

    fix compile with gcc-4.8.1

--- arch/x86_64_avx2/arch_util.h
+++ arch/x86_64_avx2/arch_util.h
@@ -38,7 +38,7 @@
  * @brief trailing zero count (count #continuous zeros from LSb)
  */
 /** immintrin.h is already included */
-#if defined(_ARCH_GCC_VERSION) && _ARCH_GCC_VERSION < 480
+#if defined(_ARCH_GCC_VERSION) && _ARCH_GCC_VERSION < 490
 #  define tzcnt(x)		( (uint64_t)__tzcnt_u64(x) )
 #else
 #  define tzcnt(x)		( (uint64_t)_tzcnt_u64(x) )
